817c6101f6fe98403ce3eb89b743f47bd71da347
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2017-04-20 19:55:43 +0300

    setup app react environment using webpack and babel
    Signed-off-by: JULIE WANJA MUGIRA <julie.mugira@andela.com>

---
+++ client/index.js
@@ -0,0 +1,9 @@
+/*
+    ./client/index.js
+*/
+// console.log('Hey guys and ladies!!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
