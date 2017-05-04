4db3931cb7ec78998f86990be1e0a26e0bd3e8b9
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2017-05-04 13:21:24 +0300

    write comments on tests
    Signed-off-by: JULIE WANJA MUGIRA <julie.mugira@andela.com>

---
+++ __tests__/login.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import shallowToJSON from 'enzyme-to-json';
+import GooglePlusLogin from '../client/components/newsly/LoginButton.js';
+
+//test the design of the login component
+describe('login button component', () => {
+    it('snapshot of GooglePlusLogin', () => {
+        const login = shallow(<GooglePlusLogin />);
+        const tree = shallowToJSON(login);
+        expect(tree).toMatchSnapshot();
+    })
+    //test there is a div containing the login button
+    it('asserts there is a googlplus button', () => {
+        const login = shallow(<GooglePlusLogin />);
+        expect(login.find('#login')).toBe.defined;
+    })
+
+    //test there is a button 
+    it('displays a login button', () => {
+        const login = shallow(<GooglePlusLogin />);
+        expect(login.find('#login').text()).toEqual('<Button />');
+    })
+})
---
+++ __tests__/sort.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import shallowToJSON from 'enzyme-to-json';
+import SortPanel from '../client/components/newsly/SortPanel.js';
+
+//test the design of the sort component
+describe('sort panel component', () => {
+    it('snapshot of SortPanel', () => {
+        const sort = shallow(<SortPanel />);
+        const tree = shallowToJSON(sort);
+        expect(tree).toMatchSnapshot();
+    })
+
+    //test there is a div containing the radiogroup
+    it('asserts there is a sort radiogroup', () => {
+        const sort = shallow(<SortPanel />);
+        expect(sort.find('#sort')).toBe.defined;
+    })
+    //test there is a top radio
+    it('displays a top radio', () => {
+        const sort = shallow(<SortPanel />);
+        expect(sort.find('.top').text()).toEqual('<Radio />');
+    })
+
+    //test there is a latest radio
+    it('displays a latest radio', () => {
+        const sort = shallow(<SortPanel />);
+        expect(sort.find('.latest').text()).toEqual('<Radio />');
+    })
+
+    //test there is a popular radio
+    it('displays a popular radio', () => {
+        const sort = shallow(<SortPanel />);
+        expect(sort.find('.popular').text()).toEqual('<Radio />');
+    })
+})
